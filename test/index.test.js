27f8d52b00057581d9dc4173089d1591460ab64b
n0cte <ilnur.a.g@gmail.com>
2021-06-07 22:42:54 +0300

    add error handling into test

--- test/index.test.js
+++ test/index.test.js
@@ -16,28 +16,36 @@ async function post(method, data) {
 describe('RPC', function () {
   describe('deploy', function () {
     it('empty contract', async function () {
-      const res = await post('deploy', {
-        source: '//SPDX-License-Identifier: MIT\n pragma solidity ^0.8.1;\n',
-        compiler: '0.8.1',
-        contract: {
-          from: '0x2e8E6cBe91e4EFAb45Ebb21b9Aef64283F26833b',
-          name: 'Owner',
-          args: [],
-        }
-      });
-      assert.ok(res.error, 'should be error');
+      try {
+        const res = await post('deploy', {
+          source: '//SPDX-License-Identifier: MIT\n pragma solidity ^0.8.1;\n',
+          compiler: '0.8.1',
+          contract: {
+            from: '0x2e8E6cBe91e4EFAb45Ebb21b9Aef64283F26833b',
+            name: 'Owner',
+            args: [],
+          }
+        });
+        assert.ok(res.error, 'should be error');
+      } catch (e) {
+        assert.fail(e.message);
+      }
     });
     it('bad source', async function () {
-      const res = await post('deploy', {
-        source: '//SPDX-License-Identifier: MIT\n pragma solidity ^0.8.1;\n contract Owner{ address owner }',
-        compiler: '0.8.1',
-        contract: {
-          from: '0x2e8E6cBe91e4EFAb45Ebb21b9Aef64283F26833b',
-          name: 'Owner',
-          args: [],
-        }
-      });
-      assert.ok(res.error, 'should be error');
+      try {
+        const res = await post('deploy', {
+          source: '//SPDX-License-Identifier: MIT\n pragma solidity ^0.8.1;\n contract Owner{ address owner }',
+          compiler: '0.8.1',
+          contract: {
+            from: '0x2e8E6cBe91e4EFAb45Ebb21b9Aef64283F26833b',
+            name: 'Owner',
+            args: [],
+          }
+        });
+        assert.ok(res.error, 'should be error');
+      } catch (e) {
+        assert.fail(e.message);
+      }
     });
     it('good source', async function () {
       const res = await post('deploy', {
